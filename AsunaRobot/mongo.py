29cb6727aefa2153f10703efa18cf69ea94dc234
Huntingbots2 <tgh191119@gmail.com>
2025-04-25 18:09:34 +0530

    Update mongo.py

--- AsunaRobot/mongo.py
+++ AsunaRobot/mongo.py
@@ -1,23 +1,15 @@
-# HuntingBots
 import asyncio
 import sys
-
 from motor import motor_asyncio
-from AsunaRobot import MONGO_DB_URI
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from AsunaRobot.confing import get_int_key, get_str_key
 
-
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "AsunaRobot"
-
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
-db = motor[MONGO_DB]
+client = MongoClient("mongodb+srv://tghcloud:Hunter01@cluster0.mzhfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+, 27017)["AsunaRobot"]
+motor = motor_asyncio.AsyncIOMotorClient("mongodb+srv://tghcloud:Hunter01@cluster0.mzhfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+, 27017)
+db = motor["AsunaRobot"]
 db = client["AsunaRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
