0be2f83e55865663bd68a5ece2207f38ef8d314b
Huntingbots2 <tgh191119@gmail.com>
2025-04-26 05:46:06 +0530

    Update dbfunctions.py
    Update function of DB

--- AsunaRobot/bot_plugins/dbfunctions.py
+++ AsunaRobot/bot_plugins/dbfunctions.py
@@ -1,14 +1,16 @@
-from typing import Dict, Union
-
+from AsunaRobot.mongo import db
+from AsunaRobot import MONGO_DB_URI
+from typing import Dict, List, Union
 
 coupledb = db.couple
 karmadb = db.karma
-
+nsfwdb = db.nsfw
 
 # Couple Chooser
 
+
 async def _get_lovers(chat_id: int):
-    lovers = await coupledb.find_one({"chat_id": chat_id})
+    lovers = coupledb.find_one({"chat_id": chat_id})
     if lovers:
         lovers = lovers["couple"]
     else:
@@ -27,42 +29,45 @@ async def get_couple(chat_id: int, date: str):
 async def save_couple(chat_id: int, date: str, couple: dict):
     lovers = await _get_lovers(chat_id)
     lovers[date] = couple
-    await coupledb.update_one(
-        {"chat_id": chat_id},
-        {"$set": {"couple": lovers}},
-        upsert=True,
-    )
+    coupledb.update_one({"chat_id": chat_id}, {"$set": {"couple": lovers}}, upsert=True)
 
 
 # Karma functions
 
 
 async def get_karmas_count() -> dict:
+    chats = karmadb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return {}
     chats_count = 0
     karmas_count = 0
-    async for chat in karmadb.find({"chat_id": {"$lt": 0}}):
+    for chat in await chats.to_list(length=1000000):
         for i in chat["karma"]:
-            karma_ = chat["karma"][i]["karma"]
-            if karma_ > 0:
-                karmas_count += karma_
+            karmas_count += chat["karma"][i]["karma"]
         chats_count += 1
     return {"chats_count": chats_count, "karmas_count": karmas_count}
 
 
 async def user_global_karma(user_id) -> int:
+    chats = karmadb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return 0
     total_karma = 0
-    async for chat in karmadb.find({"chat_id": {"$lt": 0}}):
+    for chat in await chats.to_list(length=1000000):
         karma = await get_karma(chat["chat_id"], await int_to_alpha(user_id))
-        if karma and (int(karma["karma"]) > 0):
-            total_karma += int(karma["karma"])
+        if karma:
+            if int(karma["karma"]) > 0:
+                total_karma += int(karma["karma"])
     return total_karma
 
 
 async def get_karmas(chat_id: int) -> Dict[str, int]:
-    karma = await karmadb.find_one({"chat_id": chat_id})
-    if not karma:
-        return {}
-    return karma["karma"]
+    karma = karmadb.find_one({"chat_id": chat_id})
+    if karma:
+        karma = karma["karma"]
+    else:
+        karma = {}
+    return karma
 
 
 async def get_karma(chat_id: int, name: str) -> Union[bool, dict]:
@@ -76,13 +81,11 @@ async def update_karma(chat_id: int, name: str, karma: dict):
     name = name.lower().strip()
     karmas = await get_karmas(chat_id)
     karmas[name] = karma
-    await karmadb.update_one(
-        {"chat_id": chat_id}, {"$set": {"karma": karmas}}, upsert=True
-    )
+    karmadb.update_one({"chat_id": chat_id}, {"$set": {"karma": karmas}}, upsert=True)
 
 
 async def is_karma_on(chat_id: int) -> bool:
-    chat = await karmadb.find_one({"chat_id_toggle": chat_id})
+    chat = karmadb.find_one({"chat_id_toggle": chat_id})
     if not chat:
         return True
     return False
@@ -92,14 +95,14 @@ async def karma_on(chat_id: int):
     is_karma = await is_karma_on(chat_id)
     if is_karma:
         return
-    return await karmadb.delete_one({"chat_id_toggle": chat_id})
+    return  karmadb.delete_one({"chat_id_toggle": chat_id})
 
 
 async def karma_off(chat_id: int):
     is_karma = await is_karma_on(chat_id)
     if not is_karma:
         return
-    return await karmadb.insert_one({"chat_id_toggle": chat_id})
+    return  karmadb.insert_one({"chat_id_toggle": chat_id})
 
 
 # Alpha integer
@@ -122,3 +125,27 @@ async def alpha_to_int(user_id_alphabet: str) -> int:
         user_id += str(index)
     user_id = int(user_id)
     return user_id
+
+
+"""NSFW System"""
+
+
+async def is_nsfw_on(chat_id: int) -> bool:
+    chat = nsfwdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return True
+    return False
+
+
+async def nsfw_on(chat_id: int):
+    is_nsfw = await is_nsfw_on(chat_id)
+    if is_nsfw:
+        return
+    return nsfwdb.delete_one({"chat_id": chat_id})
+
+
+async def nsfw_off(chat_id: int):
+    is_nsfw = await is_nsfw_on(chat_id)
+    if not is_nsfw:
+        return
+    return nsfwdb.insert_one({"chat_id": chat_id})
