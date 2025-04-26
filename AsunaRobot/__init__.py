6fa7058980becec61dec34f5b2a5652977dec6b4
Huntingbots2 <tgh191119@gmail.com>
2025-04-26 06:55:27 +0530

    Update __init__.py

--- AsunaRobot/__init__.py
+++ AsunaRobot/__init__.py
@@ -77,6 +77,7 @@ if ENV:
     API_HASH = os.environ.get("API_HASH", None)
     DB_URI = os.environ.get("DATABASE_URL")
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
+    MONGO_PORT = os.environ.get("MONGO_PORT", None)
     ARQ_API = os.environ.get("ARQ_API", None)
     DONATION_LINK = os.environ.get("DONATION_LINK")
     LOAD = os.environ.get("LOAD", "").split()
@@ -154,6 +155,7 @@ else:
 
     DB_URI = Config.DATABASE_URL
     MONGO_DB_URI = Config.MONGO_DB_URI
+    MONGO_PORT = Config.MONGO_PORT
     ARQ_API_KEY = Config.ARQ_API_KEY
     ARQ_API_URL = Config.ARQ_API_URL
     DONATION_LINK = Config.DONATION_LINK
