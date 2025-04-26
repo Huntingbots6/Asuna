b48dde4ac045e0323597c9928e9f9a5085564914
Huntingbots2 <tgh191119@gmail.com>
2025-04-26 06:43:22 +0530

    Update mongo.py

--- AsunaRobot/mongo.py
+++ AsunaRobot/mongo.py
@@ -1,13 +1,11 @@
-# HuntingBots
-
+#HuntingBots
 import asyncio
 import sys
-import logging as log
 
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from motor import motor_asyncio
+from AsunaRobot import MONGO_DB_URI 
+from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-
-from AsunaRobot import MONGO_DB_URI
 from AsunaRobot.services.setup import get_int_key, get_str_key
 
 
@@ -16,10 +14,11 @@ MONGO_DB_URI = get_str_key("MONGO_DB_URI")
 MONGO_DB = "AsunaRobot"
 
 
-# MongoDB client
-log.info("Initializing MongoDB client")
-mongo_client = MongoClient(MONGO_URL)
-db = mongo_client.AsunaRobot
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
+db = motor[MONGO_DB]
+db = client["AsunaRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
