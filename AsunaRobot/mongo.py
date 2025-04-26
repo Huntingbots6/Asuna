d76e58a9388f621e45f2f5e1fa29dade59f2f9db
Huntingbots2 <tgh191119@gmail.com>
2025-04-26 06:46:10 +0530

    Update mongo.py

--- AsunaRobot/mongo.py
+++ AsunaRobot/mongo.py
@@ -6,12 +6,8 @@ from motor import motor_asyncio
 from AsunaRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from AsunaRobot.services.setup import get_int_key, get_str_key
 
 
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "AsunaRobot"
 
 
 client = MongoClient()
