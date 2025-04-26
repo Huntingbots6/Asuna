9fbcfeb7df585dcc4702b97ff84593ffcf422e60
Huntingbots2 <tgh191119@gmail.com>
2025-04-26 22:39:05 +0530

    Update dbfunctions.py

--- AsunaRobot/bot_plugins/dbfunctions.py
+++ AsunaRobot/bot_plugins/dbfunctions.py
@@ -95,7 +95,7 @@ async def karma_on(chat_id: int):
 
 
 async def karma_off(chat_id: int):
-    is_karma = await is_karma_on(chat_id)
+    is_karma =  is_karma_on(chat_id)
     if not is_karma:
         return
     return await karmadb.insert_one({"chat_id_toggle": chat_id})
