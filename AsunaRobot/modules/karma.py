95ebbd60b4f1974073fb643bd615bd5c3d86e838
Huntingbots2 <tgh191119@gmail.com>
2025-04-26 21:59:53 +0530

    Update karma.py

--- AsunaRobot/modules/karma.py
+++ AsunaRobot/modules/karma.py
@@ -1,6 +1,7 @@
 import asyncio
-from AsunaRobot import pbot
+
 from pyrogram import filters 
+from AsunaRobot import pbot 
 from AsunaRobot.bot_plugins.permissions import can_change_info
 from AsunaRobot.utils.errors import capture_err
 from AsunaRobot.bot_plugins.dbfunctions import (
