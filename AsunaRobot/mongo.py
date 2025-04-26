245343791fcc57474ea9b17553848fd9ed255abe
Huntingbots2 <tgh191119@gmail.com>
2025-04-26 07:40:12 +0530

    Update mongo.py

--- AsunaRobot/mongo.py
+++ AsunaRobot/mongo.py
@@ -7,6 +7,8 @@ from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
 
+# MongoDB client
+log.info("Initializing MongoDB client")
 client = MongoClient("mongodb+srv://huntingbots:huntingbots00@cluster0.8w0fe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 , 27017)["AsunaRobot"]
 motor = motor_asyncio.AsyncIOMotorClient("mongodb+srv://huntingbots:huntingbots00@cluster0.8w0fe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
